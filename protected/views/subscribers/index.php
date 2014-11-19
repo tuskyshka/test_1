6ce285d368e149ef6166a76b9878e63357289669
vkharkov <v.kharkov@muztorg.ru>
2014-11-19 16:55:32 +0400

    Prettyfy

--- protected/views/subscribers/index.php
+++ protected/views/subscribers/index.php
@@ -1,7 +1,7 @@
 <div class="container">
 
     <form class="form-signin" name="subscribe" role="form" method="POST" action="/test_1/index.php?r=subscribers/create">
-        <h2 class="form-signin-heading">Подписаться на рассылку</h2>
+        <h2 class="form-signin-heading">Подписка на рассылку</h2>
         <label for="inputEmail">Email</label>
         <div><input type="text" name="email" id="inputEmail" class="form-control" placeholder="Email"></div>
         <br/>
@@ -12,19 +12,14 @@
 
         if (Yii::app()->user->hasFlash('success')) {
             ?>
-
                 <div class="alert alert-success"><?= Yii::app()->user->getFlash('success');?></div>
-
             <?php
         }
 
-    if (Yii::app()->user->hasFlash('error')) {
-        ?>
-
-        <div class="alert alert-danger"><?= Yii::app()->user->getFlash('error');?></div>
-
-    <?php
-    }
-
+        if (Yii::app()->user->hasFlash('error')) {
+            ?>
+                <div class="alert alert-danger"><?= Yii::app()->user->getFlash('error');?></div>
+            <?php
+            }
     ?>
 </div>

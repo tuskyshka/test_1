25f362e65799f3d246b77ec608bff999539060a2
vkharkov <v.kharkov@muztorg.ru>
2014-11-19 20:25:52 +0400

    Подписка/отписка

--- protected/controllers/SubscribersController.php
+++ protected/controllers/SubscribersController.php
@@ -45,20 +45,75 @@ class SubscribersController extends Controller
 
     public function actionSubscribe($id){
 
+        if (!Yii::app()->request->isAjaxRequest)
+            $this->redirect('/admin');
+
         $subscriber = Subscribers::model()->findByPk($id);
 
-        if ($subscriber!==NULL)
-            var_dump($subscriber);
+        if ($subscriber === NULL) {
+
+            if (Yii::app()->request->isAjaxRequest)
+                $this->renderJson('Subscriber not found', false);
+            else
+                Yii::app()->user->setFlash('subscribeError', 'Subscriber not found');
+
+        } else {
+
+            if ( $subscriber->subscribe() ) {
+
+                if (Yii::app()->request->isAjaxRequest)
+                    $this->renderJson(array('status' => Subscribers::STATUS_SUBSCRIBED, 'link' => '/subscribers/unsubscribe/'.$subscriber->id));
+                else
+                    Yii::app()->user->setFlash('subscribeSuccess', 'Пользователь '.$subscriber->email.' подписан');
+
+            } else {
+
+                if (Yii::app()->request->isAjaxRequest)
+                    $this->renderJson($subscriber->getErrors(), false);
+                else
+                    Yii::app()->user->setFlash('subscribeError', 'Ошибка подписки пользователя '.$subscriber->email);
+
+            }
+        }
+
+        $this->redirect(Yii::app()->user->returnUrl);
 
     }
 
     public function actionUnsubscribe($id){
 
+        if (!Yii::app()->request->isAjaxRequest)
+            $this->redirect('/admin');
+
         $subscriber = Subscribers::model()->findByPk($id);
 
-        if ($subscriber!==NULL)
-            $subscriber->unsubscribe();
+        if ($subscriber === NULL) {
 
+            if (Yii::app()->request->isAjaxRequest)
+                $this->renderJson('Subscriber not found', false);
+            else
+                Yii::app()->user->setFlash('subscribeError', 'Subscriber not found');
+
+        } else {
+
+            if ( $subscriber->unsubscribe() ) {
+
+                if (Yii::app()->request->isAjaxRequest)
+                    $this->renderJson(array('status' => Subscribers::STATUS_NOT_SUBSCRIBED, 'link' => '/subscribers/subscribe/'.$subscriber->id));
+                else
+                    Yii::app()->user->setFlash('subscribeSuccess', 'Пользователь '.$subscriber->email.' отписан');
+
+            } else {
+
+                if (Yii::app()->request->isAjaxRequest)
+                    $this->renderJson($subscriber->getErrors(), false);
+                else
+                    Yii::app()->user->setFlash('subscribeError', 'Ошибка отписки пользователя '.$subscriber->email);
+
+            }
+        }
+
+        $this->redirect(Yii::app()->user->returnUrl);
     }
 
 }

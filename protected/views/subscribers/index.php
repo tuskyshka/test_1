0ca4d77a76caf65845bb2622e59c35816260fbf2
vkharkov <v.kharkov@muztorg.ru>
2014-11-19 14:27:11 +0400

    Базовый шаблон и шаблон для подписки

---
+++ protected/views/subscribers/index.php
@@ -0,0 +1,11 @@
+<div class="container">
+
+    <form class="form-signin" role="form">
+        <h2 class="form-signin-heading">Подписаться на рассылку</h2>
+        <label for="inputEmail">Email</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
+        <br/>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Подписаться</button>
+    </form>
+
+</div>

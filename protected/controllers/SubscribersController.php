d3bf853e4ea284a6d2369b38891c7c93646bbc6a
vkharkov <v.kharkov@muztorg.ru>
2014-11-19 14:22:50 +0400

    Каркас контроллера подписчиков

--- protected/controllers/SubscribersController.php
+++ protected/controllers/SubscribersController.php
@@ -3,5 +3,19 @@
 class SubscribersController extends Controller
 {
 
+    public $layout = '//layouts/index';
+
+    public function actionIndex(){
+
+        $this->render('index');
+
+    }
+
+
+    public function actionCreate(){}
+
+
+    public function actionSubscribe($id){}
+    public function actionUnsubscribe($id){}
 
 }

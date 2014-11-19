80db77596ac2bfa40259c42cbb09e2320ba4a172
vkharkov <v.kharkov@muztorg.ru>
2014-11-19 16:45:10 +0400

    Обработка добавления подписчика (+ ajax)

--- protected/controllers/SubscribersController.php
+++ protected/controllers/SubscribersController.php
@@ -11,8 +11,36 @@ class SubscribersController extends Controller
 
     }
 
+    public function actionCreate(){
 
-    public function actionCreate(){}
+        $subscriber = new Subscribers();
+        $subscriber->email = Yii::app()->request->getParam('email');
+
+        if ($subscriber->validate())
+        {
+            $subscriber->save();
+
+            if (Yii::app()->request->isAjaxRequest)
+                $this->renderJson($subscriber->id);
+
+            Yii::app()->user->setFlash('success', 'Вы подписаны на рассылку');
+
+        }
+        else
+        {
+
+            if (Yii::app()->request->isAjaxRequest)
+                $this->renderJson($subscriber->getErrors(), false);
+
+            $errors = $subscriber->getErrors();
+
+            Yii::app()->user->setFlash('error', $errors['email'][0]);
+
+        }
+
+        $this->redirect('/test_1/index.php?r=subscribers/index');
+
+    }
 
 
     public function actionSubscribe($id){}

851e0ea31d7a6d255aff40954575c64ea58afb89
vkharkov <v.kharkov@muztorg.ru>
2014-11-19 16:43:58 +0400

    Добавлен вывод ошибок в шаблон подписки

--- protected/views/subscribers/index.php
+++ protected/views/subscribers/index.php
@@ -1,11 +1,30 @@
 <div class="container">
 
-    <form class="form-signin" role="form">
+    <form class="form-signin" name="subscribe" role="form" method="POST" action="/test_1/index.php?r=subscribers/create">
         <h2 class="form-signin-heading">Подписаться на рассылку</h2>
         <label for="inputEmail">Email</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
+        <div><input type="text" name="email" id="inputEmail" class="form-control" placeholder="Email"></div>
         <br/>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Подписаться</button>
     </form>
 
+    <?php
+
+        if (Yii::app()->user->hasFlash('success')) {
+            ?>
+
+                <div class="alert alert-success"><?= Yii::app()->user->getFlash('success');?></div>
+
+            <?php
+        }
+
+    if (Yii::app()->user->hasFlash('error')) {
+        ?>
+
+        <div class="alert alert-danger"><?= Yii::app()->user->getFlash('error');?></div>
+
+    <?php
+    }
+
+    ?>
 </div>
